8aeeae69b98af7a5c79fc50f18f412ea8df300c1
hyeongjeongyoo <hjeong0711@gmail.com>
2024-08-06 14:22:55 +0900

    mapper 생성 및 추가

--- src/main/java/com/tenco/bank/handler/GlobalControllerAdvice.java
+++ src/main/java/com/tenco/bank/handler/GlobalControllerAdvice.java
@@ -1,6 +1,5 @@
 package com.tenco.bank.handler;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
