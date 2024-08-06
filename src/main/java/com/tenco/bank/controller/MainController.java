2c62efcba59e8e57c2446e06632a12ef217def3f
hyeongjeongyoo <hjeong0711@gmail.com>
2024-08-06 11:14:35 +0900

    exceptionHandler 처리 완료

--- src/main/java/com/tenco/bank/controller/MainController.java
+++ src/main/java/com/tenco/bank/controller/MainController.java
@@ -1,7 +1,12 @@
 package com.tenco.bank.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.tenco.bank.handler.exception.DataDeliveryException;
+import com.tenco.bank.handler.exception.RedirectException;
 
 @Controller	// IoC 대상(싱글톤 패턴 관리가 된다.) -> 제어의 역전
 public class MainController {
@@ -23,4 +28,34 @@ public class MainController {
 		return "/main";
 	}
 	
+	// todo - 삭제 예정
+	// 주소 설계
+	// http://localhost:8080/error-test1/true
+	// http://localhost:8080/error-test1/isError
+	
+	@GetMapping("/error-test1/{isError}")
+	public String errorPage() {
+		if(true) {
+			throw new RedirectException("잘못된 요청입니다.", HttpStatus.NOT_FOUND);
+		}
+		return "main";
+	}
+	
+	// http://localhost:8080/error-test2
+	@GetMapping("/error-test2")
+	public String errorDat2() {
+		if(true) {
+			throw new DataDeliveryException("잘못된 데이터입니다.", HttpStatus.BAD_REQUEST);
+		}
+		return "main";
+	}
+	
+	// http://localhost:8080/error-test2
+	@GetMapping("/error-test3")
+	public String errorData3() {
+		if(true) {
+			throw new DataDeliveryException("인증 안된 사용자 입니다.", HttpStatus.UNAUTHORIZED);
+		}
+		return "main";
+	}
 }
