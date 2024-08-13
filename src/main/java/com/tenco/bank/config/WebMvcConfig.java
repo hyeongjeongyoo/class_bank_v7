5fe66bc77a7f1279f0138054e36077fb0488ae56
hyeongjeongyoo <hjeong0711@gmail.com>
2024-08-13 14:06:44 +0900

    사용자 암호화 처리 구현

---
+++ src/main/java/com/tenco/bank/config/WebMvcConfig.java
@@ -0,0 +1,36 @@
+package com.tenco.bank.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.tenco.bank.handler.AuthInterceptor;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+	
+	@Autowired	// DI
+	private final AuthInterceptor authInterceptor;
+	
+	// @RequiredArgsConstructor <- 생성자 대신 사용 가능
+	
+	// 우리가 만들어 놓은 AuthInterceptor 를 등록해야 한다.
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authInterceptor).addPathPatterns("/account/**")
+		.addPathPatterns("/auth/**");
+	}
+
+	@Bean // IoC 대상(싱글톤 처리)
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
